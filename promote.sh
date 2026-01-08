cbc10b90eb963c699df175c9a00b667846a5d63c
Alshaymaa Hamdan <alshaymaa.aref@hotmail.com>
2026-01-08 15:49:43 +0300

    edit deploy and promote

--- promote.sh
+++ promote.sh
@@ -27,3 +27,7 @@ docker tag $AWS_ECR_URI/cicd-shaymaa:$LATEST_TAG $AWS_ECR_URI/cicd-shaymaa:$BASE
          
 # Push Base tag
 docker push $AWS_ECR_URI/cicd-shaymaa:$BASE_TAG
+
+
+# Output for GitHub Actions
+echo "BASE_TAG=$BASE_TAG" >> "$GITHUB_OUTPUT"
