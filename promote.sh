d55534d166065da32b5daa7a5884f4e7f1c2b984
Alshaymaa Hamdan <alshaymaa.aref@hotmail.com>
2026-01-08 15:07:32 +0300

    edit promote steps

---
+++ promote.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Save latest test tag
+LATEST_TAG=$(aws ecr describe-images \
+    --repository-name cicd-aseel \
+    --region "$AWS_REGION" \
+    --output json \
+    | jq -r '
+    .imageDetails[]
+    | select(.imageTags != null)
+    | select([.imageTags[] | contains("test")] | any)
+    | {pushedAt: .imagePushedAt, tag: (.imageTags[] | select(contains("test")))}
+    ' | jq -s 'sort_by(.pushedAt) | last.tag')
+
+# Remove quotations from latest test tag
+           
+LATEST_TAG=$(echo "$LATEST_TAG" | tr -d '"')
+echo "$LATEST_TAG"
+
+# Pull Latest test tag
+docker pull ${{ secrets.AWS_ECR_URI }}/cicd-shaymaa:$LATEST_TAG
+
+# Save Base Tag
+BASE_TAG=$(echo "$LATEST_TAG" | cut -d'-' -f1 | tr -d '"')
+echo "$BASE_TAG"
+
+# retag the latest test tag
+docker tag ${{ secrets.AWS_ECR_URI }}/cicd-shaymaa:$LATEST_TAG ${{ secrets.AWS_ECR_URI }}/cicd-shaymaa:$BASE_TAG
+         
+# Push Base tag
+docker push ${{ secrets.AWS_ECR_URI }}/cicd-shaymaa:$BASE_TAG
